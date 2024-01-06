e0f4096558911cf943ba6dc6e2b4ad48c8764f50
Wildy Sheverando <153343629+wildyrando@users.noreply.github.com>
2024-01-07 01:58:07 +0700

    Create req.sh

---
+++ req.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#  JembatanProxy
+#  Alirkan seluruh akses http/https ke proxy
+#
+#  Project ini terlisensi menggunakan The MIT License
+#  Untuk informasi lebih lanjut silakan baca di https://github.com/wildyrando/JembatanProxy/LICENSE
+#
